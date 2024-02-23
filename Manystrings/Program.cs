eb835b265ff9cd820ff7018fc90b435c4281cfcf
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-23 16:02:31 -0500

    Adicionei uma string Funciona
    a

---
+++ Manystrings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "Funciona, definitivamente funciona...";
+            Console.WriteLine(mensagem);
+        }
+    }
+}
