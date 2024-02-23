ade2d47c89769059d7db817563f0124c6ceb2d50
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-23 15:21:55 -0500

    Consertei números inteiros

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,18 +6,16 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
-            sbyte numero1 = 8
-            short numero2 = 16
-            int numero3 = 32
-            long numero4 = 64
-            byte numero5 = 8
-            ushort numero6 = 16
-            uint numero7 = 32
-            ulong numero8 = 64
-            char numero9 = 16
+            sbyte numero1 = 8;
+            short numero2 = 16;
+            int numero3 = 32;
+            long numero4 = 64;
 
-            print(numero1,numero2,numero3,numero4,numero5,numero6)
-            print(numero7,numero8,numero9)
+
+            Console.WriteLine(numero1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero3);
+            Console.WriteLine(numero4);
 
         }
     }
