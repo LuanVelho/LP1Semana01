bf28352f3b805bbeef1d8f0fd7ea0e60fe37041e
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-22 21:41:59 -0500

    Escrevi ///

--- LP1/Program.cs
+++ LP1/Program.cs
@@ -2,8 +2,15 @@
 
 namespace LP1
 {
+    /// <summary>
+    /// Text .
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Bom dia, você estragou minha alegria :)
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
