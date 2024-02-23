5e2b9f63d0b6808638cf6b4bec41b7189016a6fc
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-22 22:48:45 -0500

    Adicionei numeros de tipo simples e inteiros

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte numero1 = 8
+            short numero2 = 16
+            int numero3 = 32
+            long numero4 = 64
+            byte numero5 = 8
+            ushort numero6 = 16
+            uint numero7 = 32
+            ulong numero8 = 64
+            char numero9 = 16
+
+            print(numero1,numero2,numero3,numero4,numero5,numero6)
+            print(numero7,numero8,numero9)
+
+        }
+    }
+}
