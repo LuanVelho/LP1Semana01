25f7726096be8eb22751ccefdadd073c9726969a
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-23 16:38:28 -0500

    atualização geral

--- Manystrings/Program.cs
+++ Manystrings/Program.cs
@@ -8,6 +8,23 @@ namespace ManyStrings
         {
             string mensagem = "Funciona, definitivamente funciona...";
             Console.WriteLine(mensagem);
+
+            string emojis1 = "\u265F";
+            string emojis2 = "\u2665";
+
+
+            string mensagem1 = "Bom dia" + 2;
+
+            string nome = "Luan";
+            string mensagem2 = $"Meu nome é {nome}";
+
+            Console.WriteLine(mensagem1);
+
+            Console.WriteLine(mensagem2);
+
+            Console.WriteLine(emojis1);
+            Console.WriteLine(emojis2);
+
         }
     }
 }
