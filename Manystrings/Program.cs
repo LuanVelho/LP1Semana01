47aa0b5e87eaeef5c925ea5edb017861f4672402
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-23 20:01:47 -0500

    formatei para decimal, porcentagem, Hexadecimal e moeda

--- Manystrings/Program.cs
+++ Manystrings/Program.cs
@@ -6,6 +6,18 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
+            string s1 = $"xx={xx:f2}";
+            string s2 = $"xx={xx:p1}";
+
+            string s3 = $"ii={ii:X}";
+            string s4 = $"ii={ii:C}";
+
+
+
+
             string mensagem = "Funciona, definitivamente funciona...";
             Console.WriteLine(mensagem);
 
@@ -24,6 +36,11 @@ namespace ManyStrings
 
             Console.WriteLine(emojis1);
             Console.WriteLine(emojis2);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
 
         }
     }
