59a6be37692e5b7c621a07456261b319d7d4a414
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-23 22:21:24 -0500

    atualização geral, exercicio 7 incompleto

---
+++ AskForInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Por favor escreva um número inteiro:");
+            string str = Console.ReadLine();
+            int inteiro = int.Parse(str);
+            Console.WriteLine(inteiro);
+
+            Console.WriteLine("Por favor escreva um número real");
+            string realzinho = Console.ReadLine();
+            float real = float.Parse(realzinho);
+            Console.WriteLine(real);
+
+            int resultado = inteiro + real;
+            Console.WriteLine($"{inteiro} + {real} = {resultado}");
+
+
+
+        }
+    }
+}
